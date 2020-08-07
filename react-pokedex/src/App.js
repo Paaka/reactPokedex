f68528354aac2ef8e66d7907ae46a9e0045291e8
Paaka <32105930+Paaka@users.noreply.github.com>
2020-08-08 01:41:21 +0200

    Added pokemon cards

--- react-pokedex/src/App.js
+++ react-pokedex/src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './App.css';
 import MainTemplate from './components/templates/MainTemplate/MainTamlate';
 import Axios from 'axios';
+import Card from './components/molecules/Card/Card';
+import styled from 'styled-components';
+
+const CardWrapper = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(4, 1fr);
+  grid-row-gap: 20px;
+`;
 
 class App extends React.Component {
   constructor(props) {
@@ -11,19 +20,30 @@ class App extends React.Component {
   }
 
   async fetchKantoPokemon() {
-    Axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=151')
+    Axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')
       .then(res => {
         const pokemons = res.data.results;
+        pokemons.forEach((element, index) => {
+          element.image = `https://pokeres.bastionbot.org/images/pokemon/${index + 1}.png`;
+        });
         this.setState({ loading: false, data: pokemons });
       })
       .catch(err => console.log(err));
   }
 
+  generateList() {
+    return (
+      <CardWrapper>
+        {this.state.data.map(pokemon => {
+          return <Card pokemon={pokemon} />;
+        })}
+      </CardWrapper>
+    );
+  }
+
   render() {
     const { loading, data } = this.state;
-    return (
-      <MainTemplate>{loading ? <h1>loading</h1> : <h2>loaded{console.log(data)}</h2>}</MainTemplate>
-    );
+    return <MainTemplate>{loading ? <h1>loading</h1> : this.generateList()}</MainTemplate>;
   }
 }
 
--- react-pokedex/src/components/molecules/Card/Card.js
+++ react-pokedex/src/components/molecules/Card/Card.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Card = ({ pokemen }) => (
-  <div>
-    <p>{pokemen}</p>
-    <h3>Charlizard</h3>
+const Wrapper = styled.div`
+  width: 200px;
+  height: 300px;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  justify-self: center;
+  margin-top: 10px;
+`;
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: 200px;
+`;
+
+const Card = ({ pokemon }) => (
+  <Wrapper>
+    <StyledImage src={pokemon.image} />
     <p>#003</p>
-    <button>Show more</button>
-  </div>
+    <h3>{pokemon.name}</h3>
+  </Wrapper>
 );
 
 export default Card;
